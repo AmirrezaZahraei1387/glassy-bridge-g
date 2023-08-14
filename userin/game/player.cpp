db8572a3829e2e33314f996c2122a731f1f5a530
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-14 15:18:01 -0600

    the new function named move is added to move the player

--- userin/game/player.cpp
+++ userin/game/player.cpp
@@ -1,3 +1,28 @@
 //
 // Created by KAVOSH on 8/14/2023.
 //
+#include "../../bridge/glass-pair/glassp.h"
+#include "../in/userin.h"
+#include "player.h"
+
+namespace user{
+
+    void Player::move(user::inputType input) {
+
+        switch(input){
+
+            case user::inputType::A_JUMP_RIGHT:
+
+                m_currPoint.glassPairNumber += 1;
+                m_currPoint.which = gb::GlassPair::WhichGlass::first;
+                break;
+
+            case user::inputType::D_JUMP_LEFT:
+
+                m_currPoint.glassPairNumber += 1;
+                m_currPoint.which = gb::GlassPair::WhichGlass::first;
+                break;
+        }
+    }
+
+}
--- userin/game/player.h
+++ userin/game/player.h
@@ -5,4 +5,26 @@
 #ifndef GLASSY_BRIDGE_G_PLAYER_H
 #define GLASSY_BRIDGE_G_PLAYER_H
 
+#include "../in/userin.h"
+#include "../../info.h"
+#include "../../bridge/bri/bridge.h"
+
+namespace user{
+
+    class Player{
+
+    public:
+
+        explicit Player(gb::Bridge& bridge):m_bridge{bridge}{};
+        void move(user::inputType input);
+
+        friend class gb::Bridge;
+    private:
+
+        gb::Bridge::PointBridge m_currPoint{-1};
+        gb::Bridge& m_bridge;
+        int live{info::NUMBER_USER_PLAY};
+    };
+
+}
 #endif //GLASSY_BRIDGE_G_PLAYER_H
