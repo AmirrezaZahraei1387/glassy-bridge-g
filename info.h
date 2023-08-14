8e188f3642c61bd0c8cb0cab10b935c82fd9263d
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-14 11:15:27 -0600

    the info header file containing the important informations of the project are added

---
+++ info.h
@@ -0,0 +1,11 @@
+//
+// Created by KAVOSH on 8/14/2023.
+
+#ifndef GLASSY_BRIDGE_G_INFO_H
+#define GLASSY_BRIDGE_G_INFO_H
+
+namespace info{
+    inline constexpr int BRIDGE_SIZE_LENGTH{20};
+    inline constexpr int NUMBER_USER_PLAY{5};
+}
+#endif //GLASSY_BRIDGE_G_INFO_H
